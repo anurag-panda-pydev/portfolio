81eb585aeee89b9ca6ef99f47e0d9860ab4a0019
anshubharti-dev <anshubharti.dev@gmail.com>
2026-03-29 11:59:50 +0530

    Delete assets directory

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-// Minimalist animation for fade-in
-window.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.animate-fadein').forEach((el, i) => {
-        el.style.animationDelay = (0.8 + i * 0.2) + 's';
-    });
-});
